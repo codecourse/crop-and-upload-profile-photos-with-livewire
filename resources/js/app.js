6271a040dbfd4d5cb2386732712c43c79a9c5a5c
Alex Garrett-Smith <alex@codecourse.com>
2024-02-07 09:59:43 +0000

    Episode 05

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+
+import Cropper from 'cropperjs'
+import 'cropperjs/src/css/cropper.css'
+
+window.Cropper = Cropper
--- resources/views/livewire/profile-photo-modal.blade.php
+++ resources/views/livewire/profile-photo-modal.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <img src="{{ $temporaryUrl }}">
+<div
+    class="p-10"
+    x-data="{
+        cropper: null
+    }"
+    x-init="
+        $nextTick(() => {
+            cropper = new Cropper($refs.image, {
+                autoCropArea: 1,
+                viewMode: 1,
+                aspectRatio: 1/1
+            })
+        })
+    "
+>
+    <div>
+        <img src="{{ $temporaryUrl }}" x-ref="image" class="w-full max-w-full">
+    </div>
 </div>
